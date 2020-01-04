5caaeb1788a2eb77fbdcca4e97d56682d0022d7a
Antonin <Antodu912@gmail.com>
2020-01-04 05:03:17 +0100

    first commit

---
+++ fishTankUnity/Assets/FishMovement.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class FishMovement : MonoBehaviour
+{
+    float gameX = -1;
+    float gameY = -1 ;
+
+    float fishLength = 2.3f;
+    float fishWidth = 0.8f;
+
+    public float directionX = 1;
+    public float directionY = 0;
+
+    public float speed = 0.3f;
+
+    public float borderOffset = 5f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameX == -1)
+        {
+            gameX = GameObject.Find("Ground").GetComponent<GroundBehavior>().gameX;
+            gameY = GameObject.Find("Ground").GetComponent<GroundBehavior>().gameY;
+        }
+       
+
+        //float rad = getRadian();
+        transform.position += (new Vector3(directionX, 0, directionY))*speed;
+
+        if (transform.position.x > gameX * 4)
+        {
+            updateDir(Mathf.PI);
+            //directionX = -1;
+
+        }
+        else if (transform.position.x < -(gameX * 4))
+        {
+            updateDir(0);
+            //directionX = 1;
+        }
+
+       
+        if (transform.position.z > gameY * 4)
+        {
+            updateDir(-Mathf.PI/2);
+
+        }
+        else if (transform.position.z < -(gameY * 4))
+        {
+            updateDir(Mathf.PI/2);
+        }
+
+        transform.eulerAngles = new Vector3(0, getOrientation()/2/Mathf.PI*360, 0);
+
+    }
+   
+    float getOrientation()
+    {
+        return Mathf.Atan2(directionX, directionY);
+    }
+
+    void updateDir(float angle)
+    {
+        float randAngle = Random.Range(angle - Mathf.PI/4, angle + Mathf.PI/4);
+        directionX = Mathf.Cos(randAngle);
+        directionY = Mathf.Sin(randAngle);
+}
+}
---
+++ fishTankUnity/Assets/GroundBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundBehavior : MonoBehaviour
+{
+    public float gameX = 12.5f;
+    public float gameY = 7f;
+    public bool display = true; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Game Height: " + gameX);
+        Debug.Log("Game Width: " + gameY);
+
+
+        if (display) {
+            transform.localScale = new Vector3(gameX, 1, gameY);
+        } else {
+            transform.localScale = new Vector3(0, 0, 0);
+        }
+
+        //Debug.Log(transform.localScale);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (display)
+        {
+            transform.localScale = new Vector3(gameX, 1, gameY);
+        }
+        else
+        {
+            transform.localScale = new Vector3(0, 0, 0);
+        }
+    }
+}
